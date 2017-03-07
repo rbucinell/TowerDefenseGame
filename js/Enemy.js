c7fe824a90bb885df607f486af676c778328e28f
Ryan Bucinell <rbucinell.dev@gmail.com>
2017-03-07 17:08:16 -0500

    spacing cleanup; added Id property

--- js/Enemy.js
+++ js/Enemy.js
@@ -1,101 +1,105 @@
 ////
 // Generic Enemy Class
 ////
-function Enemy()
+
+var id_index = 0; //static index incrementor for Enemy ID debugging
+
+function Enemy() 
 {
-	this.x = 0;
-	this.y = 0;
-	this.width = 20;
-	this.height = 20;
-	this.Color = "white";
-	this.Health = 10;
-	this.Speed = 3;
-	
-	this.IsMoving = false;
-	this.AtGoal = false;
-	this.Despawn = false;
-	
-	return this;
+    this.Id = ++id_index;
+    this.x = 0;
+    this.y = 0;
+    this.width = 20;
+    this.height = 20;
+    this.Color = "white";
+    this.Health = 10;
+    this.Speed = 3;
+
+    this.IsMoving = false;
+    this.AtGoal = false;
+    this.Despawn = false;
+
+    return this;
 };
 
 Enemy.prototype.setPath = function(path)
 {
-	this.Path = path.slice();
-	var startingPoint = this.Path.shift();
-	
-	this.x = startingPoint.x;
-	this.y = startingPoint.y;
+    this.Path = path.slice();
+    var startingPoint = this.Path.shift();
+
+    this.x = startingPoint.x;
+    this.y = startingPoint.y;
 }
 
 Enemy.prototype.update = function()
 {
-	console.log( "enemy update");
-	//don't move until flagged to go
-	if( !this.IsMoving )
-		return;
-	else
-		console.log( this, this.IsMoving );
-	
-	//check if dead or at goal
-	if( this.AtGoal || this.Health <= 0 )
-	{
-		this.Despawn = true;
-		return;
-	}
-	
-	var nextPoint = this.Path[0];
-	var atX = false, atY = false;
-	
-	var xdist = Math.abs( this.x-nextPoint.x);
-	if( xdist < this.Speed )
-	{
-		this.x = nextPoint.x;
-		atX = true;
-	}
-	else
-	{
-		if( this.x > nextPoint.x )
-		{
-			this.x = this.x - this.Speed;
-		}else
-		{
-			this.x = this.x + this.Speed;
-		}
-	}
-	
-	var ydist = Math.abs( this.y - nextPoint.y);
-	
-	if( ydist < this.Speed )
-	{
-		this.y = nextPoint.y;
-		atY = true;
-	}
-	else
-	{
-		if( this.y > nextPoint.y )
-		{
-			this.y = this.y - this.Speed;
-		}
-		else
-		{
-			this.y = this.y + this.Speed;
-		}
-	}
-	if( atX && atY )
-	{
-		this.Path.shift();
-		if(this.Path.length === 0)
-			this.AtGoal = true;
-	}
+    //don't move until flagged to go
+    if( !this.IsMoving )
+        return;
+    else
+        console.log( this, this.IsMoving );
+
+    //check if dead or at goal
+    if( this.AtGoal || this.Health <= 0 )
+    {
+        this.Despawn = true;
+        return;
+    }
+
+    var nextPoint = this.Path[0];
+    var atX = false, atY = false;
+
+    var xdist = Math.abs( this.x - nextPoint.x );
+    if( xdist < this.Speed )
+    {
+        this.x = nextPoint.x;
+        atX = true;
+    }
+    else
+    {
+        if( this.x > nextPoint.x )
+        {
+            this.x = this.x - this.Speed;
+        }
+        else
+        {
+            this.x = this.x + this.Speed;
+        }
+    }
+
+    var ydist = Math.abs( this.y - nextPoint.y );
+
+    if(ydist < this.Speed)
+    {
+        this.y = nextPoint.y;
+        atY = true;
+    }
+    else 
+    {
+        if( this.y > nextPoint.y )
+        {
+            this.y = this.y - this.Speed;
+        }
+        else
+        {
+            this.y = this.y + this.Speed;
+        }
+    }
+    if( atX && atY )
+    {
+        this.Path.shift();
+        if( this.Path.length === 0 )
+            this.AtGoal = true;
+    }
 }
 
 Enemy.prototype.draw = function( ctx )
 {
-	if( !this.Despawn )
-	{
-		ctx.fillStyle = this.Color
-		ctx.fillRect(this.x-this.width/2,this.y-this.height/2,this.width,this.height);
-	}
+    if( !this.Despawn)
+    {
+        ctx.fillStyle = this.Color
+        ctx.fillRect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+    }
 }
 
 ////
@@ -103,8 +107,8 @@ Enemy.prototype.draw = function( ctx )
 ////
 function RedEnemy()
 {
-	this.Color = "red";
-	this.Health = 1;
+    this.Color = "red";
+    this.Health = 1;
 }
 RedEnemy.prototype = new Enemy();
 
@@ -113,8 +117,8 @@ RedEnemy.prototype = new Enemy();
 ////
 function OrangeEnemy()
 {
-	this.Color = "orange";
-	this.Health = 2;
+    this.Color = "orange";
+    this.Health = 2;
 }
 OrangeEnemy.prototype = new Enemy();
 
@@ -122,33 +126,29 @@ OrangeEnemy.prototype = new Enemy();
 ////
 // Enemy Factory Class. Creates enemies based on input from JSON
 ////
-function EnemyFactory(){}
+function EnemyFactory() {}
 EnemyFactory.prototype.createEnemy = function( type, path )
 {
-	var parentClass = null;
-	
-	if( type === "red")
-	{
-		parentClass = RedEnemy;
-	}
-	else if( type === "orange" )
-	{
-		parentClass = OrangeEnemy;
-	}
-	else
-	{
-		parentClass = Enemy;
-	}
-	
-	//Return the class, if its not found return nothing
-	if( parentClass !== null)
-	{
-		var obj = new parentClass();
-		obj.setPath( path );
-		return obj;
-	}
-	
-	
+    var parentClass = null;
+
+    if( type === "red" )
+    {
+        parentClass = RedEnemy;
+    } 
+    else if( type === "orange" ) 
+    {
+        parentClass = OrangeEnemy;
+    } 
+    else 
+    {
+        parentClass = Enemy;
+    }
+
+    //Return the class, if its not found return nothing
+    if( parentClass !== null ) 
+    {
+        var obj = new parentClass();
+        obj.setPath( path );
+        return obj;
+    }
 }
-
-
