fb204a881a09b6ef4e1237d30dac229b628c9a02
Ryan Bucinell <rbucinell.dev@gmail.com>
2017-10-18 17:40:24 -0400

    fixed enemy spawning and despawing at different times; upgraded to e6 classes

--- td/src/js/Wave.js
+++ td/src/js/Wave.js
@@ -1,104 +1,65 @@
-//Track.js
-
-function Wave( parent, jsonobj )
+class Wave
 {
-	this.waveCounter = 0;
-	
-	this.Parent = parent;
-	this.Name = jsonobj.name;
-	this.Hint = jsonobj.hint;
-	this.Enemies = new Array();
-	this.isStarted = false;
-		
-	var factory = new EnemyFactory();
-	
-	for( var e = 0; e < jsonobj.enemies.length; e++ )
+	constructor( parent, jsonobj)
 	{
-		var quantity = jsonobj.enemies[e].qty;
-		var type = jsonobj.enemies[e].type;
+		this.waveCounter = 0;
 		
-		for( var i = 0; i < quantity; i++ )
+		this.Parent = parent;
+		this.Name = jsonobj.name;
+		this.Hint = jsonobj.hint;
+		this.Enemies = new Array();
+		this.isStarted = false;
+
+		this.delay = 100;
+		this.waveStartTime = -1;
+		
+		
+		for( var e = 0; e < jsonobj.enemies.length; e++ )
 		{
-			this.Enemies.push( factory.createEnemy( type, this.Parent.Path ));
+			var quantity = jsonobj.enemies[e].qty;
+			var type = jsonobj.enemies[e].type;
+			
+			for( var i = 0; i < quantity; i++ )
+			{
+				this.Enemies.push( EnemyFactory.createEnemy( type, this.Parent.Path ));
+			}
 		}
+		
+		this.enemyIndex = 0;
+		this.waveSpawnComplete = false;
+		this.wavelength = this.Enemies.length;
 	}
-	
-	this.enemyIndex = 0;
-	this.waveSpawnComplete = false;
-	this.wavelength = this.Enemies.length;	
-	
-	this.startWave = function()
+
+	get IsActive(){ return this.waveStartTime > 0; }
+	/**
+	 * Starts sending the wave of enemies
+	 * 
+	 * @memberof Wave 
+	 * @returns {void}
+	 */
+	startWave()
 	{
+		this.waveStartTime = game_time;
 		this.isStarted = true;
-		function spawnEnemy( wave, i )
-		{
-			wave.Enemies[i].IsMoving = true;
-		}
-		for( var i = 0; i < this.Enemies.length; i++ )
-		{
-			setTimeout( spawnEnemy( this, i ), 1000*i);
-		}
+		this.Enemies.forEach( (e, i) => {
+			e.StartTime = this.delay * i + this.waveStartTime;
+		});
 	}
-}
-/*
-	setTimeout( function(){
-		this.waveSpawnComplete = this.enemyIndex >= this.wavelength;
-	}, 1000);
-	
-	
-	console.log( this );	
-	this.waveSpawnComplete = this.enemyIndex >= this.wavelength;
-	
-	if( !this.waveSpawnComplete )
-	{
-		this.Enemies[this.enemyIndex].IsMoving = true;
-		setTimeout( this.startWave.bind( undefined, this, this.enemyIndex), 1000 );
-		this.enemyIndex++;
-	}
-	
-	/*
-	this.isStarted = true;
-	function spawnEnemy( wave, i )
-	{
-		console.log( wave.Enemies );
-		console.log( 'wave ' + i + ' started' );
-		wave.Enemies[i].IsMoving = true;
-		if( i === wave.Enemies.length -1 )
-			wave.waveSpawnComplete = true;
-	}	
-	var wave = this;
-	for( var i = 0; i < this.Enemies.length; i++ )
-	{
-		setTimeout( spawnEnemy.bind( Wave, this, i ), 1000 * i);
-	}
-	this.update();
-}*/
 
-Wave.prototype.update = function()
-{
-	if( !this.isStarted )
-		return;
-	
-	for( e in this.Enemies)
+	update()
 	{
-		if( (typeof this.Enemies[e] === 'undefined' || this.Enemies[e].AtGoal || this.Enemies[e].Health <= 0) && this.Enemies[e].Despawn == false)
-		{
-			//this.Enemies.splice(e,1);
-			this.Enemies[e].Despawn = true;
-		}
-		else
-		{
-			this.Enemies[e].update();
-		}
-		if( this.waveSpawnComplete )
-		{
-			this.Enemies.splice(e,1);
-		}
+		if( !this.isStarted )
+			return;
+		this.Enemies.forEach( (e) => {
+			if( e.StartTime < game_time )
+				e.IsMoving = true;
+			if( (typeof e === 'undefined' || e.AtGoal || e.Health <= 0) && e.Despawn == false)
+			{
+				e.Despawn = true;
+			}
+		});
+		this.Enemies = this.Enemies.filter( (el) => !el.Despawn );
+		this.Enemies.forEach( (e) => e.update());
+		
 	}
-	
-}
-
-Wave.prototype.draw = function( ctx )
-{
-	
 }
