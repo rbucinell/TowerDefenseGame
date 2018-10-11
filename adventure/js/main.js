fb8d95531e77482995fcf442b8843ea5b05a861f
Bucinell, Ryan <rbucinell.dev@gmail.com>
2018-10-11 17:32:57 -0400

    recreated main script basic starting point

--- adventure/js/main.js
+++ adventure/js/main.js
@@ -1,65 +1,34 @@
-var config = {
-    debugMode: true,
-    canvas: {
-        width: 800,
-        height: 600,
-        background: 'gray'
-    },
-    grid: {
-        rows: 60,
-        cols: 80,
-        rowHeight: 0, //Set dyanmically in setup()
-        colWidth: 0, //Set dynamically in setup()
-    }
+const cell_size = 40;
+let player;
 
-};
-var cnvs;
-var map;
 
-function setup()
-{
-    config.grid.rowHeight = config.canvas.height / config.grid.rows;
-    config.grid.colWidth = config.canvas.width / config.grid.cols;
-
-    cnvs = createCanvas( config.canvas.width + 1, config.canvas.height + 1 ); //Add a pixel to allow squares to be easily drawn
-    map = new Map( config );
-}
 
 function keyPressed( keyEvent )
 {
-    Debugger.Log( `Key ${keyEvent.keyCode} was pressed` );
-    map.keyPressed( keyEvent );
+    console.log( `Key ${keyEvent.keyCode} was pressed` );
+    player.keyPressed( keyEvent );
 }
 
+
+function setup()
+{
+    createCanvas( 1001, 601 );
+    player = new Player( 3 * cell_size, 2 * cell_size, cell_size);
+    
+}
 
 function draw()
 {
-    background( config.canvas.background );
-    stroke( 0 );
-    map.draw();
-    Debugger.DrawGrid();
-}
+    background( 'gray');
+    fill(255)
+    stroke(0);
 
-
-class Debugger
-{
-    static DrawGrid()
+    for( let i = 0; i < width; i+= cell_size)
     {
-        if( config.debugMode )
+        for( let j = 0; j < height;j += cell_size )
         {
-            for( let i = 0; i < width; i+= config.grid.square )
-            {
-                for( let j = 0; j < height; j+= config.grid.square )
-                {
-                    fill( 0,0,0,0 );
-                    stroke( 0, 0, 0, 50 )
-                    rect( i, j, config.grid.square, config.grid.square );
-                }
-            }
+            rect( i, j, cell_size, cell_size);
         }
     }
-    static Log( message )
-    {
-        console.log( message );
-    }
+    player.draw();
 }
