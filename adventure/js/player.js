73d96b927549e0f78d3a6bcf1071a57c0db78080
Bucinell, Ryan <rbucinell.dev@gmail.com>
2018-10-12 11:46:59 -0400

    move keypressed to main; cell_size is now object

--- adventure/js/player.js
+++ adventure/js/player.js
@@ -9,30 +9,6 @@ class Player
         this.item = new Item(0, 0, cell_size);
     }
 
-    keyPressed( keyEvent )
-    {
-        if( keyEvent.keyCode === LEFT_ARROW )
-        {
-            this.x -= (this.x - this.cell_size < 0) ? 0 : this.cell_size;
-        }
-        else if( keyEvent.keyCode === RIGHT_ARROW )
-        {
-            this.x += (this.x = this.cell_size > width)? 0 : this.cell_size;
-        }
-        else if( keyEvent.keyCode === DOWN_ARROW )
-        {
-            this.y += (this.y + this.cell_size *2 > height ) ? 0 : this.cell_size
-        }
-        else if( keyEvent.keyCode === UP_ARROW )
-        {
-            this.y -= (this.y - this.cell_size < 0 ) ? 0 : this.cell_size;
-        }
-        if( this.item !== null )
-        {
-            this.item.updatePositionBasedOnPlayer( this.x, this.y );
-        }
-    }
-
     update()
     {
         if( this.item != null )
@@ -47,7 +23,7 @@ class Player
         //ellipse( this.X + this.Width/2, this.Y + this.Height/2, this.gridSize * this.vision, this.gridSize * this.vision);
 
         fill( this.color );
-        rect( this.x, this.y , this.cell_size, this.cell_size );
+        rect( this.x * this.cell_size.width, this.y * this.cell_size.height, this.cell_size.width, this.cell_size.height );
         if( this.item !== null )
         {
             this.item.draw();
