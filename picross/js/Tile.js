a83055ea3c523368cd672d721fdb5370ba5fc15a
Bucinell, Ryan <rbucinell.dev@gmail.com>
2018-12-21 13:48:13 -0500

    added parameters for position and draing funciton

--- picross/js/Tile.js
+++ picross/js/Tile.js
@@ -1,7 +1,27 @@
 class Tile
 {
-    constructor()
+    constructor(x, y, s)
     {
+        this.x = x;
+        this.y = y;
+        this.s = s;
         this.state = 0;
+    }
+
+    draw()
+    {
+        switch( this.state )
+        {
+            case 0:
+                fill( 'white' );
+                break;
+            case -1:
+                fill( 'gray' );
+                break;
+            case 1:
+                fill( 'blue' );
+                break;
+        }
+        rect(this.x, this.y, this.s, this.s );
     }
 }
