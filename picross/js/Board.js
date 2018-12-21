d5673e4c9f0a54e721be08cb94499787be9735fb
Bucinell, Ryan <rbucinell.dev@gmail.com>
2018-12-21 15:13:33 -0500

    updated clues visual, drawing by tile and tile state

--- picross/js/Board.js
+++ picross/js/Board.js
@@ -14,7 +14,7 @@ class Board
             let arr = [];
             for( let j = 0; j < s; j++)
             {
-                arr.push( new Tile());
+                arr.push( new Tile(i,j,s));
             }
             this.grid.push( arr );
         }
@@ -36,6 +36,7 @@ class Board
     draw( padding )
     {
         strokeWeight( 1 );
+        stroke(0)
         let s = this.display.size;
         let gCols = this.display.cols - this.size;
         let gRows = this.display.rows - this.size;
@@ -48,38 +49,67 @@ class Board
         this.drawClues(s, gCols, gRows, padding);
 
         //Cover up
+        noStroke();
+        fill( 'WHITE');
         rect(padding, padding, gCols * s, gRows * s);
+        stroke( 0 );
 
         //Border of game area
         strokeWeight(3)
         fill(0, 0, 0, 0)
         rect(padding + gCols * s, padding + gRows * s, gridCount * s, gridCount * s);
+
+        //Draw the tiles
+        strokeWeight(1);
+        
+        for (let c = gCols; c < this.display.cols; c++)
+            for (let r = gRows; r < this.display.rows; r++)
+            {
+                let tile = this.grid[r-gRows][c-gCols];
+                let x = padding + c * s;
+                let y = padding + r * s;
+
+                stroke(0)
+                fill( tile.getColor() );
+                rect(x, y, s, s );
+
+                fill('black')
+                noStroke()                
+                text(`r:${tile.r},c:${tile.c}`, x + 5, y + 15 )
+                
+            }        
     }
 
     drawClues(s, gCols, gRows, padding) 
     {
         stroke(0);
-        fill('darkgray');
+        
         for (let c = 0; c < this.display.cols; c++) {
             for (let r = 0; r < this.display.rows; r++) {
                 let x = padding + c * s;
                 let y = padding + r * s;
                 let colClues = this.clues.cols[c - gCols];
                 let rowClues = this.clues.rows[r - gRows];
+                fill('CORNSILK');
+                noStroke()
+                rect( x+1 , y+1 , s-1, s-1 ); //just fill
 
+                fill( 'black')
                 if (c < gCols) {
                     if (r >= gRows && c <= rowClues.length) 
                         if (rowClues[c] !== null)
-                            text(`${rowClues[c]}`, x + s / 2, y + s / 2);
+                            text(`${rowClues[c]}`, x + s / 2 - textWidth(`${rowClues[c]}`)/2, y + s / 2 +textWidth(textSize()/2));
                 } 
                 else 
                 {
                     if (r < gRows && r <= colClues.length) 
                         if (colClues[r] !== null)
-                            text(`${colClues[r]}`, x + s / 2, y + s / 2);
+                            text(`${colClues[r]}`,  x + s / 2 - textWidth(`${colClues[r]}`)/2, y + s / 2 + textWidth(textSize()/2));
                 }
             }
         }
+            
+        stroke(0)
     }
 
     get ColClueVals()
@@ -97,7 +127,7 @@ class Board
     areCluesValid()
     {
         const CountGreaterThanSize = count => count > this.size;
-        
+
         let colValid = !(this.ColClueVals.some( CountGreaterThanSize ));
         let rowValid = !(this.RowClueVals.some( CountGreaterThanSize ));
         console.log( 'areCluesValid', colValid, rowValid );
