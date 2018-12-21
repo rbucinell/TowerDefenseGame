8d72ce4d9bb0c52d4f15dc93a4f76d6bc437bc5e
Bucinell, Ryan <rbucinell.dev@gmail.com>
2018-12-21 13:47:14 -0500

    cleaned out unused vars and logging

--- picross/js/main.js
+++ picross/js/main.js
@@ -1,26 +1,18 @@
 let canvasSize = 600;
 let padding = 20;
-let newGameButton;
 let gridCount = 5;
 
-let board;
-let clues = {
-    cols: [],
-    rows: []
-};
 let sizeField;
+let newGameButton;
 let colConfig, rowConfig;
-let grid = []
-let displayGrid = {
-    rows: 0,
-    cols: 0,
-    size: (canvasSize / gridCount) - padding
-}
+
+let board;
+
 const intParser = e => parseInt(e);
 
 function setup() 
 {
-    let span = createSpan("Grid Size")
+    createSpan("Grid Size")
     sizeField = createInput(gridCount, "text");
     sizeField.size(30);
 
@@ -44,28 +36,25 @@ function draw()
 function createNewGame() {
     console.log('new game clicked');
     gridCount = parseInt(sizeField.value());
-    let board = new Board( gridCount, {
+    board = new Board( gridCount, {
         cols: parseConfigToArray(colConfig, gridCount),
         rows: parseConfigToArray(rowConfig, gridCount)
     });
     board.setDisplaySize( canvasSize, padding );
-    console.log( 'board valid: ' + board.isValid());
     return board;
 }
 
 function parseConfigToArray(inputField, s) {
     let ret = [];
     let strArr = inputField.value().split(' ');
-    for (let i = 0; i < s; i++) {
-        if (i == 2)
-            console.log(strArr[i].split(',').map(intParser));
-        if (strArr[i] === undefined || strArr[i] === "0")
-            ret.push([]);
-        else
-            ret.push(strArr[i].split(',').map(intParser));
+    for (let i = 0; i < s; i++) {   
+        if (i >= strArr.length || strArr[i] === undefined || strArr[i] === "0")
+                ret.push([]);
+            else
+                ret.push(strArr[i].split(',').map(intParser));
 
     }
-    let maxLen = ret.reduce(reducer, 0);
+    let maxLen = ret.reduce((acc, cur) => Math.max(acc, cur.length), 0);
     ret.forEach(span => {
         while (span.length < maxLen) {
             span.unshift(null);
